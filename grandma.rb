0e1f305122b18de2037ed116b8644548de76add4
Amanda Wingate <arw0219@gmail.com>
2018-03-04 02:45:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,12 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(speak)
-  if speak == speak.upcase
-    "NO, NOT SINCE 1938!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase 
+    return "NO, NOT SINCE 1938!"
   else
-     "SPEAK UP, SONNY!"
+     return "HUH?! SPEAK UP, SONNY!"
   end
 end
