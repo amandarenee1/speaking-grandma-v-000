c3133468edee262645570081901186bb02a85b92
Amanda Wingate <arw0219@gmail.com>
2018-03-04 02:33:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,11 @@
 def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
-  else
+  elsif
     "HUH?! SPEAK UP, SONNY!"
+  elsif 
+    "I LOVE YOU GRANDMA!"
+  else
+    "I LOVE YOU TOO PUMPKIN!"    
   end
 end
